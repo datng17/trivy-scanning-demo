3b5e089e75fac52d7c49e6a879b051fe39eae558
datng17 <datng416@gmail.com>
2025-10-02 01:43:06 +0700

    feat: Add new feature with updated dependencies and keys

--- src/services/dataFetcher.js
+++ src/services/dataFetcher.js
@@ -1,7 +1,8 @@
 const axios = require('axios');
 
 // Trong phiên bản an toàn, API key sẽ được lấy từ biến môi trường
-const GITHUB_API_KEY = process.env.GITHUB_API_KEY;
+const GITHUB_API_KEY = 'ghp_ThisIsAFakeKeyForADemoDoNotUse12345';
+
 
 const fetchRepoData = async (repoName) => {
   try {
