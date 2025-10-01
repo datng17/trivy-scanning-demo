45387557c70bb1a39995aab70425f04749aca095
datng17 <datng416@gmail.com>
2025-10-02 02:00:01 +0700

    change API key

--- src/services/dataFetcher.js
+++ src/services/dataFetcher.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 // Trong phiên bản an toàn, API key sẽ được lấy từ biến môi trường
-const GITHUB_API_KEY = process.env.GITHUB_API_KEY;
+const GITHUB_API_KEY = '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';
 
 const fetchRepoData = async (repoName) => {
   try {
